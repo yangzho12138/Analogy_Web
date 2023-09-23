dc057345e3172a4bc396b431bda40d3cd5bd6c71
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-09-23 13:43:21 -0500

    common

--- common/build/events/payment_complete_event.d.ts
+++
@@ -1,9 +0,0 @@
-import { Subjects } from "./subjects";
-export interface PaymentCompleteEvent {
-    subject: Subjects.PaymentComplete;
-    data: {
-        id: string;
-        orderId: string;
-        chargeId: string;
-    };
-}
