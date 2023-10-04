b1dd5defbe1413055ef78f5ba41bb7b6c21ab5d4
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-10-04 12:39:01 -0500

    custom error update

--- common/src/errors/custom-error.ts
+++ common/src/errors/custom-error.ts
@@ -1,5 +1,3 @@
-// 用抽象类统一格式和防止typo：继承的每一个error class都需要有statusCode和serializeErrors()
-// 抽象类可以被instanceof识别（error handler中），接口不行
 export abstract class CustomError extends Error{
     abstract statusCode : number;
 
