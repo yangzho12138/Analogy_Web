91db92d539e346e33967d9ebfc9afd190139fd81
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-10-18 15:31:24 -0500

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const test = async() => {
+    console.log('test');
+    const {data} = await axios.post('/api/users/signout', {
+      'email': 'yz@illinois.edu',
+      'password': '123456'
+    });
+    console.log(data);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +25,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        <div onClick={test}>test</div>
         </a>
       </header>
     </div>
